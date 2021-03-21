e228284e7ffd61fe85f2739b1370a211b57aa7c1
Andreas <info@azettl.net>
2021-03-21 23:55:04 +0100

    layout changes

--- options.php
+++ options.php
@@ -56,14 +56,7 @@
 
             echo '<div class="wrap wrap-magazine">
                     <h1>
-                        Magazine Options 
-                        <i>
-                            developed with 
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                            </svg> 
-                            by <a href="https://azettl.net" target="_blank" rel="noopener">Andreas Zettl</a>
-                        </i>
+                        Magazine Options
                     </h1>
                     <form name="magazine_options_form" method="post">
                         <table class="form-table">
