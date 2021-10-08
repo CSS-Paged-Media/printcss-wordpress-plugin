83d9ee62a6de49d19f9a2dc850a9e428bb2036e9
Andreas <info@azettl.net>
2021-10-09 00:12:30 +0200

    show settings link first

--- options.php
+++ options.php
@@ -190,5 +190,5 @@
     add_filter('plugin_action_links_magazine/magazine.php', function(array $aLinks){
         $aLinks[] = '<a href="' . admin_url( 'options-general.php?page=magazine_option_page' ) . '">' . __('Settings') . '</a>';
         
-        return $aLinks;
+        return array_reverse($aLinks);
     });
